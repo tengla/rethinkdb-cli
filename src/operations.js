0b0c4c304ee453cfe5ad8155d5cc946dad0d8642
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-02-04 22:15:16 +0100

    Major refactor

--- src/operations.js
+++ src/operations.js
@@ -2,7 +2,45 @@
 
 const R = require('rethinkdb');
 
+const queryFilter = function (query, filter) {
+
+    if (!filter) {
+        return query;
+    }
+
+    filter = filter.split(':');
+
+    const key = filter[0];
+    const val = filter[1];
+    const _filter = {};
+
+    if (!isNaN(val)) {
+        _filter = {};
+        _filter[key] = Number(val).valueOf();
+        return query.filter(_filter);
+    }
+
+    if (val.match(/[\.\+\?\*\^\$]/)) {
+        return query.filter( (doc) => {
+
+            return doc(key).match(val);
+        });
+    }
+
+    _filter[key] = val;
+    return query.filter(_filter);
+};
+
 module.exports = {
+
+    get: function (name,id) {
+
+        R.table(name).get(id).run(this.conn).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+        return this;
+    },
 
     tableList: function () {
 
@@ -39,11 +77,13 @@ module.exports = {
 
             this.fire('error', [err.msg]);
         });
+        return this;
     },
 
     dbDrop: function (name) {
 
         R.dbDrop(name).run(this.conn).then(this.defaultResolver.bind(this), this.defaultRejecter.bind(this));
+        return this;
     },
 
     tableCreate: function (name) {
@@ -54,6 +94,7 @@ module.exports = {
                 this.defaultResolver.bind(this),
                 this.defaultRejecter.bind(this)
             );
+        return this;
     },
 
     tableDrop: function (name) {
@@ -64,36 +105,46 @@ module.exports = {
                 this.defaultResolver.bind(this),
                 this.defaultRejecter.bind(this)
             );
+        return this;
     },
 
-    table: function (name, filter) {
+    table: function (name, filter, count, limit, skip) {
 
         if (!name) {
             this.fire('error', ['I need a table name to do that']);
             return this;
         }
+
         let query = R.table(name);
 
-        if (filter) {
-            filter = filter.split(':');
-            const _filter = {};
-            _filter[filter[0]] = filter[1];
-            query = query.filter(_filter);
+        query = queryFilter(query,filter);
+
+        if (count) {
+            query = query.count().run(this.conn).then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+            return this;
+        }
+
+        if (skip && limit) {
+
+            skip = Number(skip).valueOf();
+            limit = Number(limit).valueOf();
+
+            query = query.slice(skip, skip + limit);
+        }
+        else if (limit) {
+            query = query.limit(Number(limit).valueOf());
         }
 
         query.run(this.conn).then( (cursor) => {
 
-            cursor.toArray().then( (items) => {
-
-                this.fire('message', [JSON.stringify(items, null, 4)]);
-            }, (err) => {
-
-                this.fire('error', [JSON.stringify(err)]);
-            });
-        }, (err) => {
-
-            this.fire('error', [err.msg]);
-        });
+            cursor.toArray().then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+        }, this.defaultRejecter.bind(this));
 
         return this;
     },
@@ -134,11 +185,18 @@ module.exports = {
         return this;
     },
 
-    find: function (id) {
+    use: function (name) {
 
-        const msg = ['Find', id].join(' ');
-        this.fire('message', [msg]);
-        return this;
+        this.conn.use(name);
+        this.fire('message', ['Using ' + name]);
+    },
+
+    help: function () {
+
+        const message = 'Possible commands are '
+            + Object.keys(this.operations).join(', ');
+
+        this.fire('message', [message]);
     },
 
     quit: function () {
