455642c79f52a92c8617ef99a3c37438ac08142a
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-02-04 14:44:17 +0100

    Use 'minimize' as arg parser and 'joi' for validation

--- bin/repl.js
+++ bin/repl.js
@@ -2,6 +2,50 @@
 
 const Readline = require('readline');
 const Commander = require('../index').Commander;
+const Joi = require('joi');
+const Color = require('colors');
+const Args = require('minimist')(process.argv.slice(2));
+
+const schema = Joi.object().keys({
+    db: Joi.string().required(),
+    host: Joi.string().required(),
+    port: Joi.number(),
+    '_': Joi.array()
+}).with('db','host','_');
+
+const errorMessages = {
+    db: 'No db specified'.red,
+    host: 'No host specified'.red
+};
+
+const usage = [
+    'Usage node repl.js',
+    '',
+    'DB OPTIONS:',
+    '',
+    '--host <host> --port <port> --db <name> --auth_key <auth_key>'
+];
+
+Joi.validate(Args, schema, function(err,val) {
+    if (err) {
+        const messages = err.details.map ( (detail) => {
+            return errorMessages[detail.path];
+        });
+        console.log('\n' + messages.join('\n') + '\n');
+        console.log(usage.join('\n'));
+        process.exit(1);
+    }
+});
+
+const Config = function (args) {
+
+    const keys = ['host','port','db','auth_key'];
+    const conf = {};
+    keys.forEach( (k) => {
+        conf[k] = args[k];
+    });
+    return conf;
+};
 
 const rl = Readline.createInterface({
     input: process.stdin,
@@ -14,7 +58,7 @@ const rl = Readline.createInterface({
 });
 
 const command = new Commander()
-  .connect({ db: process.argv[2] });
+  .connect(Config(Args));
 
 const Quit = () => {
     console.log('\nbye ...\n');
@@ -44,7 +88,7 @@ command.on('connect', (message) => {
 
 rl.on('line', (cmd) => {
     cmd = cmd.trim();
-    command.exec(cmd);
+    command.execString(cmd);
 });
 
 command.on('quit', Quit);
