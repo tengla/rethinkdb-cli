353f812648b459bb0770548362672bc96afd44d3
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-02-06 22:47:58 +0100

    use vm sandbox instead of eval

--- bin/faker.js
+++ bin/faker.js
@@ -4,9 +4,21 @@ const Joi = require('joi');
 const Args = require('minimist')(process.argv.slice(2));
 const Colors = require('colors/safe');
 const Faker = require('faker');
-const safeeval = eval;
+const Vm = require('vm');
+const Fs = require('fs');
 
-Faker.name; // keep stoopid linter happy ;)
+(function () {
+
+    if( ! Fs.existsSync('.fakerrc') ) {
+        return false;
+    }
+    const contents = Fs.readFileSync('.fakerrc', 'utf8');
+    const rc = JSON.parse(contents);
+
+    Args['_'] = Args['_'].length > 0 && Args['_'] || rc.args;
+    Args['name'] = Args['name'] || rc.name;
+    Args['n'] = Args['n'] || rc.n;
+})();
 
 const schema = Joi.object().keys({
     n: Joi.number().required(),
@@ -16,7 +28,7 @@ const schema = Joi.object().keys({
 
 const errorMessages = {
     n: Colors.red('number of items must be given'),
-    name: Colors.red('object name must be given'),
+    name: Colors.red('fixture name must be given'),
     '_': Colors.red('Faker methods must be given')
 };
 
@@ -24,10 +36,10 @@ const usage = [
     'Usage:',
     '',
     'node ./bin/faker.js --name people -n 5 \\',
-    '\t\'name=Faker.name.findName()\' \\',
-    '\t\'age=Faker.random.number(34)\' \\',
-    '\t\'address=Faker.address.streetAddress()\' \\',
-    '\t\'city=Faker.address.city()\'',
+    '\t\'name=name.findName()\' \\',
+    '\t\'age=random.number(34)\' \\',
+    '\t\'address=address.streetAddress()\' \\',
+    '\t\'city=address.city()\'',
     ''
 ];
 
@@ -38,25 +50,39 @@ Joi.validate(Args, schema, (err,val) => {
 
             return errorMessages[detail.path];
         });
-        console.log('\n' + messages.join('\n') + '\n');
+        
+        if (messages.length > 0) {
+            console.log(messages);
+            console.log('\n' + messages.join('\n') + '\n');  
+        }
+
         console.log(usage.join('\n'));
         process.exit(1);
     }
 });
 
-const objects = Array.apply(null, [Args.n]).fill(0).map( (item, idx) => {
+const expressions = Args._.map( (arg) => {
 
-    const object = {};
-    Args._.forEach( (arg) => {
-
-        const parts = arg.split('=');
-        object[parts[0]] = safeeval(parts[1]); // nevermind that it's dangerous.
-    });
-    return object;
+    const kv = arg.split('=');
+    return '\'' + kv[0] + '\':Faker.' + kv[1];
 });
 
-const pack = {};
+const code = 'this.fn=function () { return {' + expressions.join(',') +'}}; this.toJSON();';
 
-pack[Args.name] = objects;
+const sandbox = Vm.createContext({
+    Faker: Faker,
+    n: Args.n || 5,
+    name: Args.name || 'fixture',
+    fn: null,
+    toJSON: function () {
+        const packed = {};
+        packed[this.name] = Array.apply(null, [this.n]).fill(0).map( (item, idx) => {
+            return this.fn();
+        });
+        return JSON.stringify(packed, null, 4);
+    }
+});
 
-console.log(JSON.stringify(pack,null,4));
+var script = new Vm.Script(code);
+var result = script.runInContext(sandbox);
+console.log(result);
