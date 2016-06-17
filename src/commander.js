b06905926125febbe6de4011de4f1f409d12eb79
Thomas Eng <thomas.tinnesand.eng@gmail.com>
2016-06-18 00:21:37 +0200

    I've sinned.
    This is far from being an atomic commit.

--- src/commander.js
+++ src/commander.js
@@ -2,6 +2,7 @@
 
 const R = require('rethinkdb');
 const Colors = require('colors/safe');
+const ParseFilter = require('./parsefilter');
 
 const Commander = function (config) {
 
@@ -21,14 +22,242 @@ Commander.prototype.connect = function (config) {
 
         this.conn = c;
         this.emit('connect', 'Connected to ' + this.status());
-    }, (err) => {
-
-        this.emit('error', err.message);
     });
     return this;
 };
 
-Commander.prototype.operations = require('./operations');
+Commander.prototype.operations = {
+
+    changes: function (name) {
+
+        R.table(name).changes().run(this.conn).then( (cursor) => {
+
+            cursor.each( (err,item) => {
+
+                if (err) {
+                    return this.emit('error', err.msg);
+                }
+                this.emit('change', name, item);
+            });
+        }, this.defaultRejecter);
+        return this;
+    },
+
+    get: function (name,id) {
+
+        R.table(name).get(id).run(this.conn).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+        return this;
+    },
+
+    dbList: function () {
+
+        R.dbList().run(this.conn).then( (list) => {
+
+            const message = this.conn.host + ' has dbs \'' + list.join('\', \'') + '\'';
+            this.emit('message', message);
+        }, (err) => {
+
+            this.emit('error', err.msg);
+        });
+        return this;
+    },
+
+    dbCreate: function (name) {
+
+        R.dbCreate(name).run(this.conn).then( (result) => {
+
+            this.emit('message', JSON.stringify(result, null, 4));
+        }, (err) => {
+
+            this.emit('error', err.msg);
+        });
+        return this;
+    },
+
+    dbDrop: function (name) {
+
+        R.dbDrop(name).run(this.conn).then(this.defaultResolver.bind(this), this.defaultRejecter.bind(this));
+        return this;
+    },
+
+    delete: function (name) {
+
+        R.table(name).delete().run(this.conn).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+        return this;
+    },
+
+    indexCreate: function (table,name) {
+
+        R.table(table).indexCreate(name).run(this.conn).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+        return this;
+    },
+
+    indexDrop: function (table,name) {
+
+        R.table(table).indexDrop(name).run(this.conn).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+        return this;
+    },
+
+    indexStatus: function (table) {
+
+        R.table(table).indexStatus().run(this.conn).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+        return this;
+    },
+
+    insert: function (object,returnChanges) {
+
+        if (!object) {
+            this.emit('error', 'No objects specified for \'' + name + '\'');
+            return this;
+        }
+
+        const tables = Object.keys(object);
+
+        const promises = tables.map( (table) => {
+
+            return R.table(table).insert(object[table],{ returnChanges: returnChanges }).run(this.conn);
+        });
+
+        Promise.all(promises).then(
+            this.defaultResolver.bind(this),
+            this.defaultRejecter.bind(this)
+        );
+
+        return this;
+    },
+
+    table: function (name, filter, count, limit, skip) {
+
+        if (!name) {
+            this.emit('error', 'I need a table name to do that');
+            return this;
+        }
+
+        let query = R.table(name);
+
+        if (filter) {
+
+            const fn = new ParseFilter().exec(filter);
+            query = query.filter(fn);
+        }
+
+        if (count) {
+            query = query.count().run(this.conn).then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+            return this;
+        }
+
+        if (skip && limit) {
+
+            skip = Number(skip).valueOf();
+            limit = Number(limit).valueOf();
+
+            query = query.slice(skip, skip + limit);
+        }
+        else if (limit) {
+            query = query.limit(Number(limit).valueOf());
+        }
+
+        query.run(this.conn).then( (cursor) => {
+
+            cursor.toArray().then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+        }, this.defaultRejecter.bind(this));
+
+        return this;
+    },
+
+    tableCreate: function (name) {
+
+        R.tableCreate(name)
+            .run(this.conn)
+            .then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+        return this;
+    },
+
+    tableDrop: function (name) {
+
+        R.tableDrop(name)
+            .run(this.conn)
+            .then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+        return this;
+    },
+
+    tableList: function () {
+
+        R.tableList().run(this.conn).then( (list) => {
+
+            const message = list.length > 0 ? '\n' + this.conn.db
+                + ' has tables:\n\n\t \''
+                + list.join('\', \'')
+                + '\'\n' : this.conn.db + ' is empty';
+
+            return this.emit('message', message);
+        }, (err) => {
+
+            return this.emit('error', err.msg);
+        });
+        return this;
+    },
+
+    update: function (table,id,json) {
+
+        const data = JSON.parse(json);
+        R.table(table)
+            .get(id)
+            .update(data, { returnChanges: true })
+            .run(this.conn)
+            .then(
+                this.defaultResolver.bind(this),
+                this.defaultRejecter.bind(this)
+            );
+        return this;
+    },
+
+    use: function (name) {
+
+        this.conn.use(name);
+        this.emit('message', 'Using ' + name);
+    },
+
+    help: function () {
+
+        const message = 'Possible commands are '
+            + Object.keys(this.operations).join(', ');
+
+        this.emit('message', message);
+    },
+
+    quit: function () {
+
+        this.emit('quit');
+    }
+};
 
 Commander.prototype.defaultResolver = function (result) {
 
@@ -59,8 +288,7 @@ Commander.prototype.exec = function () {
 Commander.prototype.execString = function (line) {
 
     const args = line.split(' ');
-
-    return this.exec.call(this, args);
+    return this.exec.apply(this, args);
 };
 
 Commander.prototype.emit = function () {
@@ -68,10 +296,7 @@ Commander.prototype.emit = function () {
     const name = arguments[0];
     const args = Array.prototype.slice.call(arguments,1);
     const method = this.observers[name];
-    if (!method) {
-        return null;
-    }
-    method.apply(null,args);
+    return method && method.apply(null,args);
 };
 
 Commander.prototype.on = function (name, callback) {
@@ -82,12 +307,12 @@ Commander.prototype.on = function (name, callback) {
 
 Commander.prototype.status = function () {
 
-    let message = 'Not connected';
-    if (this.conn) {
-        message = this.conn.host + ':' + this.conn.port;
-        message = message + (this.conn.db ? ('/' + this.conn.db) : '');
+    try {
+        return this.conn.host + ':' + this.conn.port + '/' + this.conn.db;
     }
-    return message;
+    catch (err) {
+        return 'Not connected';
+    }
 };
 
 module.exports = Commander;
--- test/commander.js
+++ test/commander.js
@@ -5,23 +5,20 @@ const expect = Code.expect;
 const Lab = require('lab');
 const Commander = require('../index').Commander;
 const lab = exports.lab = Lab.script();
-const Fs = require('fs');
 let commander;
 
 lab.experiment('Commander', () => {
 
     lab.beforeEach( (done) => {
-        commander = new Commander();        
+
+        commander = new Commander();
         done();
     });
 
-    lab.afterEach( (done) => {
-        commander.close();
-        done();
-    });
+    lab.test('connect', (done) => {
 
-    lab.test('connects', (done) => {
         commander.on('connect', (msg) => {
+
             expect(msg).to.match(/Connected to/);
             done();
         });
@@ -29,18 +26,125 @@ lab.experiment('Commander', () => {
     });
 
     lab.test('does not connect', (done) => {
-        
+
         commander.connect = function () {
+
             this.emit('error', 'mocking this');
         };
 
         commander.close = () => {};
 
         commander.on('error', (msg) => {
+
             expect(msg).to.match(/mocking this/);
             done();
         });
 
         commander.connect({ db: 'test' });
+    });
+
+    lab.test('it has status \'not connected\'', (done) => {
+
+        commander.conn = null;
+        expect(commander.status()).to.equal('Not connected');
+        done();
+    });
+
+    lab.test('it has status connected', (done) => {
+
+        commander.on('connect', () => {
+
+            expect(commander.status()).to.equal('localhost:28015/test');
+            done();
+        });
+        commander.connect({ db: 'test' });
+    });
+
+    lab.test('execString', (done) => {
+
+        commander.on('message', (list) => {
+
+            expect(list).to.be.string();
+            done();
+        });
+
+        commander.on('connect', (msg) => {
+
+            commander.execString('tableList');
+        });
+
+        commander.connect({ db: 'test' });
+    });
+
+    lab.test('emit', (done) => {
+
+        expect(commander.emit('bogus', 1,2,3)).to.be.undefined();
+        done();
+    });
+
+    lab.test('exec', (done) => {
+
+        expect(commander.exec('bogus', 1,2,3)).to.equal(commander);
+        done();
+    });
+
+    lab.test('defaultResolver', (done) => {
+
+        expect(commander.defaultResolver({ name: 'bogus' })).to.be.undefined();
+        done();
+    });
+
+    lab.test('defaultRejecter', (done) => {
+
+        expect(commander.defaultRejecter({ name: 'bogus' })).to.be.undefined();
+        done();
+    });
+
+    lab.test('close', (done) => {
+
+        commander.connect({ db: 'test' });
+        commander.on('connect', () => {
+
+            commander.close();
+            done();
+        });
+    });
+
+    lab.test('quit', (done) => {
+
+        commander.operations.quit.call(commander);
+        done();
+    });
+
+    lab.test('use', (done) => {
+
+        commander.connect({ db: 'test' });
+
+        commander.on('message', (msg) => {
+
+            expect(msg).to.equal('Using test');
+            done();
+        });
+
+        commander.on('connect', () => {
+
+            commander.operations.use.call(commander, 'test');
+        });
+    });
+
+    lab.test('help', (done) => {
+
+        commander.operations.help.call(commander);
+        done();
+    });
+
+    lab.test('tableList', (done) => {
+
+        commander.on('connect', (msg) => {
+
+            expect(commander.operations.tableList.call(commander)).to.equal(commander);
+            done();
+        });
+        commander.connect({ db: 'bogus' });
     });
 });
